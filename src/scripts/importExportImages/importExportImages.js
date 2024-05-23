b12980e17a4d1068b6072ecf9de4e60534da75f7
Paul <159176858+pauljgonzalez@users.noreply.github.com>
2024-05-22 20:18:16 -0400

    Changed miningPNG

--- src/scripts/importExportImages/importExportImages.js
+++ src/scripts/importExportImages/importExportImages.js
@@ -1,5 +1,5 @@
 import villagePNG from '../../images/placeholder_images/village.png';
 import woodCuttingPNG from '../../images/placeholder_images/woodcutting.png';
-import miningPNG from '../../images/placeholder_images/woodcutting.png';
+import miningPNG from '../../images/placeholder_images/mining.png';
 
 export { villagePNG, woodCuttingPNG, miningPNG };
