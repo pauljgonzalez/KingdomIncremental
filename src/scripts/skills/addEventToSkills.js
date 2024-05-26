d45a77e442cea25325f71f3668716b7a698a86c6
Paul <159176858+pauljgonzalez@users.noreply.github.com>
2024-05-26 17:02:40 -0400

    Added remove skill main screen on skill sidebar click

--- src/scripts/skills/addEventToSkills.js
+++ src/scripts/skills/addEventToSkills.js
@@ -1,12 +1,14 @@
 import { editCurrentSkillName } from '../editCurrentSkillDOM/editSkillName';
 import { editCurrentSkillDesc } from '../editCurrentSkillDOM/editDesc';
 import { editCurrentImage } from '../editCurrentSkillDOM/editImage';
+import { removeMainScreenInfo } from '../editMainInfoDOM/removeMainChildren';
 
 const skillArray = document.querySelectorAll('.skill');
 
 export function addEventsToSkillsList() {
   skillArray.forEach((skill) => {
     skill.addEventListener('click', () => {
+      removeMainScreenInfo();
       editCurrentSkillName(skill.innerHTML);
       editCurrentSkillDesc(skill);
       editCurrentImage(skill.innerHTML);
